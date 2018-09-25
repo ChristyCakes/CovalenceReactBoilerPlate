5db7fea315acb80fc528a98db45018b0e18a446d
Christy Welsh <christyannwelsh@gmail.com>
2018-09-24 21:28:48 -0700

    display one chirp

--- client/src/components/chirp.jsx
+++ client/src/components/chirp.jsx
@@ -1,30 +1,46 @@
+// Chirp component requests and displays single chirp
+
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import 'isomorphic-fetch';
 
 class Chirp extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            user: "",
+            text: ""
+        }
+    }
 
     componentDidMount() {
         fetch(`http://127.0.0.1:3000/api/chirps/${this.props.match.params.id}`)
-            .then ()
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    user: data.user,
+                    text: data.text
+                })
+            })
             .catch(err => console.log(err))
     }
 
     render() {
         return (
-
             <div>
                 <Fragment>
                     <Link to="/">Home</Link>
                 </Fragment>
-        <div>one chirp here</div>
-
+                <div className="current">
+                    <div className="flex-column">
+                        <div className='chirps'>
+                            <p>{this.state.user}: {this.state.text}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
-
-        
-        
     }
 }
 
